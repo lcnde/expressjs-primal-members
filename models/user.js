e1e64d063ece557315e4d9840ce930a6cd9d54cf
lcnde <176673026+lcnde@users.noreply.github.com>
2022-08-11 12:05:41 +0200

    fix: changes to user schema

--- models/user.js
+++ models/user.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema(
   {
-    username: {type: String, required: true, maxLength: 100, unique: true},
-    password: {type: String, required: true, maxLength: 24},
-    is_member: {type: Boolean, required: true},
+    username: {type: String, required: true, maxLength: 24, unique: true},
+    password: {type: String, required: true, maxLength: 100},
+    is_member: {type: Boolean},
     is_admin: {type: Boolean, required: true},
   }
 );
