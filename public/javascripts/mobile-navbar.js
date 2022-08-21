86ce183d980bcd16f1a4634911b9c7549460cc4d
lcnde <176673026+lcnde@users.noreply.github.com>
2022-08-21 06:37:58 +0200

    fix: added no-scrolling on mobile navbar

---
+++ public/javascripts/mobile-navbar.js
@@ -0,0 +1,9 @@
+const cb = document.getElementById('menu-trigger');
+cb.addEventListener('click', event => {
+  // console.log(cb.checked);
+  if (cb.checked === true) {
+    document.body.classList.add('no-scrolling');
+  } else {
+    document.body.classList.remove('no-scrolling')
+  };
+});
