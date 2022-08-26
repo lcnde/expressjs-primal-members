e4c008d0d230012f7b6a9d40bf4a59b43fcb503b
lcnde <176673026+lcnde@users.noreply.github.com>
2022-08-26 06:17:41 +0200

    fte: signup page style

--- routes/auth.js
+++ routes/auth.js
@@ -34,7 +34,7 @@ router.get('/signup', function(req, res, next) {
   res.render('signup',
     {
       title: 'Signup',
-      stylesheetName: 'index.css'
+      stylesheetName: 'signup.css'
     }
   );
 });
